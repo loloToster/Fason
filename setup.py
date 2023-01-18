412b275becf899353f4fddb24835384ee32debf6
loloToster <lolo.toster4@gmail.com>
2023-01-18 11:43:20 +0100

    🩹 changed description type

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
   author="loloToster",
   description="Use json files quicker and easier with this python package!",
   long_description=readme,
+  long_description_content_type="text/markdown",
   keywords=["python", "json"],
   packages=["fason"]
 )
